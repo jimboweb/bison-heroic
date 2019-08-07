b7338ff9a3581badaba8eadbfc2c150c9e6e45e4
jimboweb <jimboweb@gmail.com>
2019-08-07 15:31:30 -0400

    just some comments

--- src/util/sortUtil.js
+++ src/util/sortUtil.js
@@ -1,8 +1,21 @@
+/**
+ * naive algorithm to compare two algorithms using kendall-tau
+ * this has reduces that make a bigger array than the original,
+ * if that bothers you look away
+ * @param a first array
+ * @param b second array
+ * @returns integer of kendall-tau comparison
+ */
 const kendallTau = (a,b)=>{
   if(a.length!==b.length){
     throw new Error("in kendall tau algoritm arrays must be same length");
   }
 
+  /**
+   * compares each number to each other number to find integer comparison for each pair
+   * @param arr
+   * @returns LARGER array (size: triangular(arr.length)) with integer comparison for each pair
+   */
   const compareAll= (arr) => arr.reduce(
     (accumOuter,itemOuter,i)=>
 
