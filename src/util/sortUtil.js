70c840773a16b9a2cdfd955ebf9773b1e459d702
jimboweb <jimboweb@gmail.com>
2019-08-07 15:20:59 -0400

    kendallTau works now

--- src/util/sortUtil.js
+++ src/util/sortUtil.js
@@ -1,34 +1,40 @@
-module.exports.kendallTau = (a,b)=>{
+const kendallTau = (a,b)=>{
   if(a.length!==b.length){
     throw new Error("in kendall tau algoritm arrays must be same length");
   }
 
   const compareAll= (arr) => arr.reduce(
     (accumOuter,itemOuter,i)=>
-      accumOuter.push(
+
+      accumOuter.concat(
         arr.filter(
-        (_void,j)=>
-          j!==i
-      )
-        .reduce(
-          (accumInner, itemInner)=>
-            accumInner.push(itemOuter===itemInner?
-              0:
-              itemOuter<itemInner?
-                1:-1)
+          (_void,j)=>
+            j!==i
         )
+          .reduce(
+            (accumInner, itemInner)=>
+            {
+              accumInner.push(itemOuter===itemInner?
+                0:
+                itemOuter<itemInner?
+                  1:-1)
+              return accumInner;
+            },[]
+          )
       )
-  )
+    ,[]
+  );
 
   const bCompare = compareAll(b);
   return compareAll(a)
     .map(
       (aItem,i)=>
-        (a===0 || b === 0)?
+        (aItem===0 || bCompare[i] === 0)?
           0:
-          a<b?-1:1
+          aItem===bCompare[i]?0:1
 
     ).reduce(
-      (accum,item)=>accum+item
+      (accum,item)=>
+        accum+item
     )
 };
